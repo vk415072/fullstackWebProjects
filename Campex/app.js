3f1ec4321edec1a837bcf1d1554be8a92904a8f3
Vivek Sengar <36097100+vk415072@users.noreply.github.com>
2021-05-12 21:37:28 +0530

    C of CRUD
    added a page to add new campground to db

--- Campex/app.js
+++ Campex/app.js
@@ -23,6 +23,9 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+// 10. to parse req.body in app.post, we need to fake it
+app.use(express.urlencoded({ extended: true }));
+
 // 2. listening to homepage req and rendering it.
 app.get("/", (req, res) => {
   res.render("home");
@@ -39,6 +42,20 @@ app.get("/", (req, res) => {
 app.get("/campgrounds", async (req, res) => {
   const campgrounds = await campground.find({});
   res.render("campgrounds/index", { campgrounds });
+});
+
+// 8. new campground page
+app.get("/campgrounds/new", async (req, res) => {
+  //   const campground1 = await campground.findById(req.params.id);
+  res.render("campgrounds/new");
+});
+
+// 9. receiving new campground data and adding to db
+app.post("/campgrounds", async (req, res) => {
+//   console.log(req.body.campground);
+  const newCampground = new campground(req.body.campground);
+  await newCampground.save();
+  res.redirect(`/campgrounds/${newCampground._id}`);
 });
 
 // 7. individual campgrounds show page
