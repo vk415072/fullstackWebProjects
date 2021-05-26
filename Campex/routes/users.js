a62654108d6f8c95a1d59c37c847f8638de20422
Vivek Sengar <36097100+vk415072@users.noreply.github.com>
2021-05-26 23:12:49 +0530

    added automatic login after register

--- Campex/routes/users.js
+++ Campex/routes/users.js
@@ -11,14 +11,19 @@ router.get("/register", (req, res) => {
 
 router.post(
    "/register",
-   catchAsync(async (req, res) => {
+   catchAsync(async (req, res, next) => {
       try {
          const { email, username, password } = req.body;
          const newUser = new User({ email, username });
          const registeredUser = await User.register(newUser, password);
-
-         req.flash("success", "Welcome to Campex!");
-         res.redirect("/campgrounds");
+         // logging in the registered user after the registration
+         req.login(registeredUser, (err) => {
+            if (err) {
+               return next(err);
+            }
+            req.flash("success", "Welcome to Campex!");
+            res.redirect("/campgrounds");
+         });
       } catch (e) {
          req.flash("error", e.message);
          res.redirect("register");
