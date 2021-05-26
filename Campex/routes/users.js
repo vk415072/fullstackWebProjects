4d2e903cfad80206866edad063bd812d68e21f15
Vivek Sengar <36097100+vk415072@users.noreply.github.com>
2021-05-26 15:08:25 +0530

    added login page
    using passport to store pass

--- Campex/routes/users.js
+++ Campex/routes/users.js
@@ -1,13 +1,40 @@
 const express = require("express");
+const passport = require("passport");
 const router = express.Router();
 const User = require("../models/user");
+const catchAsync = require("../helpers/catchAsync");
 
+// register routes
 router.get("/register", (req, res) => {
    res.render("users/register.ejs");
 });
 
-router.post("/register", async (req, res) => {
-   res.send(req.body);
+router.post(
+   "/register",
+   catchAsync(async (req, res) => {
+      try {
+         const { email, username, password } = req.body;
+         const newUser = new User({ email, username });
+         const registeredUser = await User.register(newUser, password);
+
+         req.flash("success", "Welcome to Campex!");
+         res.redirect("/campgrounds");
+      } catch (e) {
+         req.flash("error", e.message);
+         res.redirect("register");
+      }
+   })
+);
+
+// login routes
+router.get("/login", (req, res) => {
+   res.render("users/login");
+});
+
+// using passport default middleware here with local strategy and some more features
+router.post("/login", passport.authenticate("local", { failureFlash: true, failureRedirect: "/login" }), (req, res) => {
+   req.flash("success", "Welcome back!");
+   res.redirect("/campgrounds");
 });
 
 module.exports = router;
