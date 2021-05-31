d2a7f13d06f98235aa2a7d5d091135c0028a0805
Vivek Sengar <36097100+vk415072@users.noreply.github.com>
2021-05-31 12:00:02 +0530

    now showing campground location to added map

--- Campex/public/js/mapboxJS.js
+++ Campex/public/js/mapboxJS.js
@@ -4,6 +4,10 @@ mapboxgl.accessToken = mapboxToken;
 const map = new mapboxgl.Map({
    container: "map",
    style: "mapbox://styles/mapbox/streets-v11",
-   center: [76.812438, 29.9388],
-   zoom: 4,
+   // center: [99.812438, 30.9388],
+   center: campground.geometry.coordinates,
+   zoom: 9,
 });
+
+// adding marker
+new mapboxgl.Marker().setLngLat(campground.geometry.coordinates).addTo(map);
