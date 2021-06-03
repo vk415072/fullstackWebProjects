ce1c1af32947ac747a5e1db68fb267d2f8479cf8
Vivek Sengar <36097100+vk415072@users.noreply.github.com>
2021-06-03 20:24:50 +0530

    prevent cross site scripting with joi

--- Campex/helpers/joiSchema.js
+++ Campex/helpers/joiSchema.js
@@ -1,14 +1,42 @@
 // 1. requiring Joi
-const Joi = require("joi");
+const BaseJoi = require("joi");
+// 6. requiring sanitize-html
+const sanitizeHTML = require("sanitize-html");
 
+// 5. creating a rule with joi to stop any html input form user side
+// (from joi docs)
+const extension = (joi) => ({
+   type: "string",
+   base: joi.string(),
+   messages: {
+      "string.escapeHTML": "{{#label}} must not include HTML!",
+   },
+   rules: {
+      escapeHTML: {
+         validate(value, helpers) {
+            const clean = sanitizeHtml(value, {
+               allowedTags: [],
+               allowedAttributes: {},
+            });
+            if (clean !== value) return helpers.error("string.escapeHTML", { value });
+            return clean;
+         },
+      },
+   },
+});
+
+// 7. not previous joi is not base joi and this new joi is the extended version of this extension above
+const Joi = BaseJoi.extend(extension);
+
+// 8. now using escapeHTML in every input field
 // 2. copied schema from app.js comment: 57.
 module.exports.joiCampgroundSchema = Joi.object({
    campground: Joi.object({
-      title: Joi.string().required(),
+      title: Joi.string().required().escapeHTML(),
       price: Joi.number().required().min(0),
-      location: Joi.string().required(),
+      location: Joi.string().required().escapeHTML(),
       // image: Joi.string().required(),
-      description: Joi.string().required(),
+      description: Joi.string().required().escapeHTML(),
    }).required(),
    deleteImages: Joi.array(),
 });
@@ -20,6 +48,6 @@ module.exports.joiCampgroundSchema = Joi.object({
 module.exports.reviewSchema = Joi.object({
    review: Joi.object({
       rating: Joi.number().required().min(1).max(5),
-      body: Joi.string().required(),
+      body: Joi.string().required().escapeHTML(),
    }).required(),
 });
